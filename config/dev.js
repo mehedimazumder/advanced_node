401a980d834c1825445f3af0eb941af13e5bfc12
mehedi mazumder <mehedi.mazumder1275@gmail.com>
2021-12-25 00:22:15 +0600

    image upload at aws s3

--- config/dev.js
+++ config/dev.js
@@ -6,4 +6,6 @@ module.exports = {
     "mongodb://mehedi:mehedi@nodeadv-shard-00-00.bv4zy.mongodb.net:27017,nodeadv-shard-00-01.bv4zy.mongodb.net:27017,nodeadv-shard-00-02.bv4zy.mongodb.net:27017/nodeAdv?ssl=true&replicaSet=atlas-ia17dx-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
+  accessKeyId: "AKIAIWSXFHRM7F6Z3NWQ",
+  secretAccessKey: "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",
 };
